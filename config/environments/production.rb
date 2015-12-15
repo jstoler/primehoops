46ad7739fe15ba0f34bb3ec673936195f1a00790
Josh Stoler <jstoler3131@gmail.com>
2015-12-15 14:27:48 -0500

    solve for img to heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
   # Required for Heroku
   # Note to set this to the actual host name
   config.action_mailer.default_url_options = { host: 'mbahoops.heroku.com' }
+
+  config.serve_static_assets = true
+  config.assets.compile = true
 end
